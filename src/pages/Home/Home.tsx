ebc20432ff297a91bfcb4e60700be3bb6a3ed050
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2025-01-19 03:08:50 +0530

    feat(home): developed the UI for hotels viewing

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,109 @@
-import {View, Text, Button, SafeAreaView} from 'react-native';
+import React from 'react';
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+} from 'react-native';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client';
 
-const Home: React.FC = () => {
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/',
+  cache: new InMemoryCache(),
+});
+
+const GET_HOTELS = gql`
+  query {
+    hotels {
+      hotel_id
+      hotel_name
+      city
+      country
+      star_rating
+      rates_from
+      rates_currency
+      photo1
+    }
+  }
+`;
+
+const Home = () => {
+  const {loading, error, data} = useQuery(GET_HOTELS);
+  console.log('error', error);
+  console.log('data', data);
+  console.log('loading', loading);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error: {error.message}</Text>;
+
   return (
     <SafeAreaView>
-      <View>
-        <Text>Home Screen</Text>
-      </View>
+      <FlatList
+        data={data.hotels}
+        keyExtractor={item => item.hotel_id.toString()}
+        renderItem={({item}) => (
+          <View style={styles.card}>
+            <Image source={{uri: item.photo1}} style={styles.image} />
+            <Text style={styles.title}>{item.hotel_name}</Text>
+            <Text style={styles.subtitle}>
+              {item.city}, {item.country} - {item.star_rating}⭐
+            </Text>
+            <Text style={styles.price}>
+              From {item.rates_from} {item.rates_currency}
+            </Text>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
 
-export default Home;
+const App = () => (
+  <ApolloProvider client={client}>
+    <View style={{flex: 1, padding: 10}}>
+      <Home />
+    </View>
+  </ApolloProvider>
+);
+
+const styles = StyleSheet.create({
+  card: {
+    marginBottom: 20,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 5,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#555',
+  },
+  price: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#00aaff',
+  },
+});
+
+export default App;
